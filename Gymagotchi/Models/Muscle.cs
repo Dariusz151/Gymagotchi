f3c643ae7817b6ab5af2bb724576d219b7df67f3
Kwidzinski <mateusz.kwidzinski@intel.com>
2019-10-07 14:24:02 +0200

    Avatar part 1

---
+++ Gymagotchi/Models/Muscle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gymagotchi.Models
+{
+    public  class Muscle
+    {
+        public string Name { get; set; }
+        public Decimal Size { get; set; }
+        public Decimal LoadPerWeek { get; set; }
+        public bool Exhausted { get; set; }
+        public bool CriticalExhausted { get; set; }
+    }
+}
