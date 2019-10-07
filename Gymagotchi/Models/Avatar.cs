2c3c487252c2ba518b814f803c24fe870b07448d
Kwidzinski <mateusz.kwidzinski@intel.com>
2019-10-07 10:10:18 +0200

    Avatar part 1

--- Gymagotchi/Models/Avatar.cs
+++ Gymagotchi/Models/Avatar.cs
@@ -7,6 +7,26 @@ namespace Gymagotchi.Models
 {
     public class Avatar
     {
+        public string Name { get; set; }
+        public Decimal Weight { get; set; }
+        public Decimal Height { get; set; }
+        public List<Muscle> LeftArm { get; set; }
+        public List<Muscle> RightArm { get; set; }
+        public List<Muscle> LeftLeg { get; set; }
+        public List<Muscle> RightLeg { get; set; }
+        public List<Muscle> Chest { get; set; }
+        public List<Muscle> Back { get; set; }
+        public List<Muscle> Stomach { get; set; }
 
+        public Avatar()
+        {
+            LeftArm = new List<Muscle>
+            {
+                new Muscle{Name="Biceps"},
+                new Muscle{Name="Triceps"},
+                new Muscle{Name="Forehand"},
+                new Muscle{Name="UpperShoulder"}
+            };
+        }
     }
 }
